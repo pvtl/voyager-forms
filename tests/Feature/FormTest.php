0cded010def6a74c6b33ed28ae8491c544710cbd
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-14 15:35:25 +1000

    Test scaffold for Forms

---
+++ tests/Feature/FormTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pvtl\VoyagerForms\Tests\Feature;
+
+class FormTest extends \Tests\TestCase
+{
+    public function testIfFormRendersShortcodeToTheFrontend()
+    {
+    }
+
+    public function testIfFormSubmitsInputFieldsCorrectly()
+    {
+    }
+
+    public function testIfFormSubmissionCreatesAnEnquiry()
+    {
+    }
+}
---
+++ tests/Unit/FormTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pvtl\VoyagerForms\Tests\Unit;
+
+class FormTest extends \Tests\TestCase
+{
+    public function testIfFormHasInputsAssigned()
+    {
+    }
+
+    public function testIfFormHasAViewAssigned()
+    {
+    }
+
+    public function testIfFormHasAMailToAddress()
+    {
+    }
+
+    public function testIfFormHasASubmissionHook()
+    {
+    }
+}
