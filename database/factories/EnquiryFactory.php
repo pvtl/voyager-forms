aa7a8e46a21e07f75d0e52fe0f690e1819b91539
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-16 08:56:21 +1000

    Create factory for Enquiry

---
+++ database/factories/EnquiryFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(\Pvtl\VoyagerForms\FormEnquiry::class, function (Faker $faker) {
+    return [
+        'form_id' => factory(\Pvtl\VoyagerForms\Form::class)->create()->id,
+        'data' => json_encode([
+            'first_name' => 'Jonathon',
+            'last_name' => 'Spoons',
+            'gender' => 'Chameleon',
+            'attitude' => 'Savage',
+        ]),
+        'mailto' => $faker->safeEmail,
+        'hook' => $faker->url,
+    ];
+});
