d37ae8f82e969573a0e0186e5bce20ad35dd16f9
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-15 15:35:11 +1000

    Revert "Do not need this seeder"
    This reverts commit 739df5d04a68e118c19141e74ae9897412c965ab.

---
+++ database/seeds/FormsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pvtl\VoyagerForms\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use Pvtl\VoyagerForms\Form;
+
+class FormsTableSeeder extends Seeder
+{
+    /**
+     * Auto generated seed file.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
