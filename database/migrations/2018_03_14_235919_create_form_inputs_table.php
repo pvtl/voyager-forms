5b25d00bd5785ef4f36745458c342a615163f4d1
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-21 10:49:22 +1000

    NULLABLE creation fields

--- database/migrations/2018_03_14_235919_create_form_inputs_table.php
+++ database/migrations/2018_03_14_235919_create_form_inputs_table.php
@@ -16,10 +16,10 @@ class CreateFormInputsTable extends Migration
         Schema::create('form_inputs', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('form_id');
-            $table->string('label');
+            $table->string('label')->nullable()->default('');
             $table->string('class')->nullable()->default('');
             $table->enum('type', ['text', 'number', 'email', 'text_area', 'checkbox', 'select', 'radio']);
-            $table->boolean('required');
+            $table->boolean('required')->nullable()->default(false);
             $table->timestamps();
         });
     }
