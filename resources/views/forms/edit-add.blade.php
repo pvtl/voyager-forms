e08f90281003f34fb7950da51e972c9c2c8cd5a3
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-23 14:39:49 +1000

    Fix form layout for edit-add blade

--- resources/views/forms/edit-add.blade.php
+++ resources/views/forms/edit-add.blade.php
@@ -17,8 +17,9 @@
     <div class="page-content edit-add container-fluid">
         @include('voyager::alerts')
 
-        @if (isset($form->id))
-            <div class="row">
+        <div class="row">
+            @if (isset($form->id))
+
                 <div class="col-md-3 col-lg-2">
                     <div class="panel panel-bordered panel-primary">
                         <div class="panel-heading">
@@ -94,131 +95,133 @@
                         </div> <!-- /.panel-body -->
                     </div> <!-- /.panel -->
                 </div>
+            @endif
 
-                <div class="col-md-10">
-                    <div class="panel panel-bordered panel-info">
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
 
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Form Details</h3>
-                            <div class="panel-actions">
-                                <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse"
-                                   aria-hidden="true"></a>
-                            </div> <!-- /.panel-actions -->
-                        </div> <!-- /.panel-heading -->
-
-                        <div class="panel-body">
-                            <form
-                                role="form"
-                                action="@if (isset($form->id))
-                                {{ route('voyager.'.$dataType->slug.'.update', $form->id) }}
-                                @else
-                                {{ route('voyager.'.$dataType->slug.'.store') }}
-                                @endif"
-                                method="POST"
-                                enctype="multipart/form-data">
-
-                                {{ csrf_field() }}
-
-                                @if (isset($form->id))
-                                    {{ method_field("PUT") }}
-                                @endif
-                                <div class="form-group">
-                                    <label for="title">Title</label><br>
-                                    <input name="title" class="form-control" type="text"
-                                           @if (isset($form->title)) value="{{ $form->title }}" @endif required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="mailto">Mail To (Separate multiple with ',')</label><br>
-                                    <input name="mailto" class="form-control" type="text"
-                                           @if (isset($form->mailto)) value="{{ $form->mailto }}" @endif required>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="hook">Event Hook (Fires after form is submitted)</label><br>
-                                    <input name="hook" class="form-control" type="text"
-                                           @if (isset($form->hook)) value="{{ $form->hook }}" @endif>
-                                </div>
-
-                                <div class="panel-footer">
-                                    <button type="submit"
-                                            class="btn btn-primary">{{ __('voyager.generic.submit') }}</button>
-                                </div>
-                            </form>
+            <div class="col-md-10">
+                <div class="panel panel-bordered panel-info">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
+                    @endif
+
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Form Details</h3>
+                        <div class="panel-actions">
+                            <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse"
+                               aria-hidden="true"></a>
+                        </div> <!-- /.panel-actions -->
+                    </div> <!-- /.panel-heading -->
+
+                    <div class="panel-body">
+                        <form
+                            role="form"
+                            action="@if (isset($form->id))
+                            {{ route('voyager.'.$dataType->slug.'.update', $form->id) }}
+                            @else
+                            {{ route('voyager.'.$dataType->slug.'.store') }}
+                            @endif"
+                            method="POST"
+                            enctype="multipart/form-data">
+
+                            {{ csrf_field() }}
+
+                            @if (isset($form->id))
+                                {{ method_field("PUT") }}
+                            @endif
+                            <div class="form-group">
+                                <label for="title">Title</label><br>
+                                <input name="title" class="form-control" type="text"
+                                       @if (isset($form->title)) value="{{ $form->title }}" @endif required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="mailto">Mail To (Separate multiple with ',')</label><br>
+                                <input name="mailto" class="form-control" type="text"
+                                       @if (isset($form->mailto)) value="{{ $form->mailto }}" @endif required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="hook">Event Hook (Fires after form is submitted)</label><br>
+                                <input name="hook" class="form-control" type="text"
+                                       @if (isset($form->hook)) value="{{ $form->hook }}" @endif>
+                            </div>
+
+                            <div class="panel-footer">
+                                <button type="submit"
+                                        class="btn btn-primary">{{ __('voyager.generic.submit') }}</button>
+                            </div>
+                        </form>
                     </div>
+                </div>
 
-                    @if (isset($form))
-                        @foreach ($form->inputs as $input)
-                            <div class="panel panel-bordered panel-info">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">{{ ucfirst($input->type) }} Input</h3>
-                                    <div class="panel-actions">
-                                        <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse"
-                                           aria-hidden="true"></a>
-                                    </div> <!-- /.panel-actions -->
-                                </div> <!-- /.panel-heading -->
+                @if (isset($form))
+                    @foreach ($form->inputs as $input)
+                        <div class="panel panel-bordered panel-info">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">{{ ucfirst($input->type) }} Input</h3>
+                                <div class="panel-actions">
+                                    <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse"
+                                       aria-hidden="true"></a>
+                                </div> <!-- /.panel-actions -->
+                            </div> <!-- /.panel-heading -->
 
-                                <div class="panel-body">
-                                    <form role="form" action="{{ route('voyager.inputs.update', $input->id) }}"
-                                          method="POST"
-                                          enctype="multipart/form-data">
-                                        {{ csrf_field() }}
-                                        {{ method_field("PUT") }}
+                            <div class="panel-body">
+                                <form role="form" action="{{ route('voyager.inputs.update', $input->id) }}"
+                                      method="POST"
+                                      enctype="multipart/form-data">
+                                    {{ csrf_field() }}
+                                    {{ method_field("PUT") }}
 
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="label">Input Label</label>
-                                                    <input name="label" class="form-control" id="label"
-                                                           value="{{ $input->label }}">
-                                                </div>
-                                            </div>
-
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="class">Input Class</label>
-                                                    <input name="class" class="form-control" id="class"
-                                                           value="{{ $input->class }}">
-                                                </div>
-                                            </div>
-
-                                            <div class="col-md-6 col-lg-5">
-                                                <div class="form-group">
-                                                    <input
-                                                        type="checkbox"
-                                                        name="required"
-                                                        id="required"
-                                                        data-name="required"
-                                                        class="toggleswitch"
-                                                        value="1"
-                                                        data-on="Yes" {{ $input->required ? 'checked="checked"' : '' }}
-                                                        data-off="No"
-                                                    />
-                                                    <label for="required"> &nbsp;Input Required</label>
-                                                </div> <!-- /.form-group -->
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="label">Input Label</label>
+                                                <input name="label" class="form-control" id="label"
+                                                       value="{{ $input->label }}">
                                             </div>
                                         </div>
 
-                                        <input type="hidden" name="input_id" value="{{ $input->id }}"/>
-                                        <button type="submit"
-                                                class="btn btn-success btn-sm">{{ __('Update This Input') }}</button>
-                                    </form>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="class">Input Class</label>
+                                                <input name="class" class="form-control" id="class"
+                                                       value="{{ $input->class }}">
+                                            </div>
+                                        </div>
 
-                                    <form method="POST" action="{{ route('voyager.inputs.destroy', $input->id) }}">
-                                        {{ method_field("DELETE") }}
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <div class="col-md-6 col-lg-5">
+                                            <div class="form-group">
+                                                <input
+                                                    type="checkbox"
+                                                    name="required"
+                                                    id="required"
+                                                    data-name="required"
+                                                    class="toggleswitch"
+                                                    value="1"
+                                                    data-on="Yes" {{ $input->required ? 'checked="checked"' : '' }}
+                                                    data-off="No"
+                                                />
+                                                <label for="required"> &nbsp;Input Required</label>
+                                            </div> <!-- /.form-group -->
+                                        </div>
+                                    </div>
 
-                                        <span class="btn-group-xs">
+                                    <input type="hidden" name="input_id" value="{{ $input->id }}"/>
+                                    <button type="submit"
+                                            class="btn btn-success btn-sm">{{ __('Update This Input') }}</button>
+                                </form>
+
+                                <form method="POST" action="{{ route('voyager.inputs.destroy', $input->id) }}">
+                                    {{ method_field("DELETE") }}
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                    <span class="btn-group-xs">
                                             <button
                                                 data-delete-input-btn
                                                 type="submit"
@@ -226,14 +229,13 @@
                                                 class="btn btn-danger btn-xs delete"
                                             >{{ __('voyager.generic.delete') }} This Input</button>
                                         </span>
-                                    </form>
-                                </div> <!-- /.panel-body -->
-                            </div>
-                        @endforeach
-                    @endif
-                </div>
+                                </form>
+                            </div> <!-- /.panel-body -->
+                        </div>
+                    @endforeach
+                @endif
             </div>
-        @endif
+        </div>
     </div>
 @stop
 
