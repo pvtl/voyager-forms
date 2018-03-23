2e3e6ff02471b73ce78626b160476d50ad0c3aec
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-23 15:38:17 +1000

    We have rendering dynamic elements... not all are working however

--- resources/views/forms/render.blade.php
+++ resources/views/forms/render.blade.php
@@ -1,14 +1,41 @@
-<form action="{{ route('voyager.enquiries.store') }}" method="POST">
+<form action="{{ route('voyager.enquiries.store') }}" method="POST" id="{{ $form->title }}">
     {{ csrf_field() }}
 
     <input type="hidden" name="id" value="{{ $form->id }}">
 
     @foreach ($form->inputs as $input)
+        @php
+            $row = $input;
+            $row->field = $row->label;
+
+            $options = $input->options;
+        @endphp
+
         <div class="{{ $input->class }}">
             <label for="{{ $input->label }}">{{ $input->label }}</label>
-            <input name="{{ $input->label }}" type="{{ $input->type }}" @if ($input->required) required @endif>
+
+            @if (in_array($input->type, ['text', 'number', 'email']))
+                @include('voyager::formfields.text')
+            @endif
+
+            @if ($input->type === 'text_area')
+                @include('voyager::formfields.text_area')
+            @endif
+
+            @if ($input->type === 'checkbox')
+                @include('voyager::formfields.checkbox')
+            @endif
+
+            @if ($input->type === 'select')
+                @include('voyager::formfields.select_dropdown')
+            @endif
+
+            @if ($input->type === 'radio')
+                @include('voyager::formfields.radio_btn')
+            @endif
+
         </div>
     @endforeach
 
-    <button type="submit" value="submit">Submit</button>
+    <button type="submit" value="submit" class="btn btn-primary">Submit</button>
 </form>
