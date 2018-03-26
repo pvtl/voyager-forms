77b35334463b280992a106180beb7ccd78952965
Matt Mcnamee <matt@mcnamee.co>
2018-03-26 15:32:55 +1000

    Adjusting layout of form builder

--- resources/views/forms/edit-add.blade.php
+++ resources/views/forms/edit-add.blade.php
@@ -20,46 +20,65 @@
         <div class="row">
             @if (isset($form->id))
 
-                <div class="col-md-3 col-lg-2">
-                    <div class="panel panel-bordered panel-primary">
+                <div class="col-md-4">
+                    <div class="panel">
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
                         <div class="panel-heading">
-                            <h3 class="panel-title">Add Field</h3>
-                            <div class="panel-actions">
-                                <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse"
-                                   aria-hidden="true"></a>
-                            </div> <!-- /.panel-actions -->
+                            <h3 class="panel-title">Form Details</h3>
                         </div> <!-- /.panel-heading -->
 
                         <div class="panel-body">
-                            <form role="form" action="{{ route('voyager.inputs.store') }}" method="POST"
-                                  enctype="multipart/form-data">
+                            <form
+                                role="form"
+                                action="@if (isset($form->id))
+                                {{ route('voyager.'.$dataType->slug.'.update', $form->id) }}
+                                @else
+                                {{ route('voyager.'.$dataType->slug.'.store') }}
+                                @endif"
+                                method="POST"
+                                enctype="multipart/form-data">
+
                                 {{ csrf_field() }}
 
+                                @if (isset($form->id))
+                                    {{ method_field("PUT") }}
+                                @endif
                                 <div class="form-group">
-                                    <label for="type">Field Type</label>
-                                    <select class="form-control" name="type" id="type">
-                                        <option value="">-- Select --</option>
+                                    <label for="title">Title</label><br>
+                                    <input name="title" class="form-control" type="text"
+                                           @if (isset($form->title)) value="{{ $form->title }}" @endif required>
+                                </div>
 
-                                        @foreach (config('voyager-forms.available_inputs') as $key => $value)
-                                            <option value="{{ $key }}">{{ $value }}</option>
-                                        @endforeach
-                                    </select>
-                                </div> <!-- /.form-group -->
+                                <div class="form-group">
+                                    <label for="mailto">Mail To (Separate multiple with ',')</label><br>
+                                    <input name="mailto" class="form-control" type="text"
+                                           @if (isset($form->mailto)) value="{{ $form->mailto }}" @endif required>
+                                </div>
 
-                                <input type="hidden" name="form_id" value="{{ $form->id }}"/>
+                                <div class="form-group">
+                                    <label for="hook">Event Hook (Fires after form is submitted)</label><br>
+                                    <input name="hook" class="form-control" type="text"
+                                           @if (isset($form->hook)) value="{{ $form->hook }}" @endif>
+                                </div>
+
                                 <button type="submit"
-                                        class="btn btn-success btn-sm">{{ __('voyager.generic.add') }}</button>
+                                        class="btn btn-primary">{{ __('voyager.generic.submit') }}</button>
                             </form>
-                        </div> <!-- /.panel-body -->
+                        </div>
                     </div>
 
-                    <div class="panel panel-bordered panel-warning">
+                    <div class="panel">
                         <div class="panel-heading">
                             <h3 class="panel-title">Form Layout</h3>
-                            <div class="panel-actions">
-                                <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse"
-                                   aria-hidden="true"></a>
-                            </div> <!-- /.panel-actions -->
                         </div> <!-- /.panel-heading -->
 
                         <div class="panel-body">
@@ -94,72 +113,39 @@
                             </form>
                         </div> <!-- /.panel-body -->
                     </div> <!-- /.panel -->
+
+                    <div class="panel panel-bordered panel-warning">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Add Field</h3>
+                        </div> <!-- /.panel-heading -->
+
+                        <div class="panel-body">
+                            <form role="form" action="{{ route('voyager.inputs.store') }}" method="POST"
+                                  enctype="multipart/form-data">
+                                {{ csrf_field() }}
+
+                                <div class="form-group">
+                                    <label for="type">Field Type</label>
+                                    <select class="form-control" name="type" id="type">
+                                        <option value="">-- Select --</option>
+
+                                        @foreach (config('voyager-forms.available_inputs') as $key => $value)
+                                            <option value="{{ $key }}">{{ $value }}</option>
+                                        @endforeach
+                                    </select>
+                                </div> <!-- /.form-group -->
+
+                                <input type="hidden" name="form_id" value="{{ $form->id }}"/>
+                                <button type="submit"
+                                        class="btn btn-success btn-sm">{{ __('voyager.generic.add') }}</button>
+                            </form>
+                        </div> <!-- /.panel-body -->
+                    </div>
                 </div>
             @endif
 
 
-            <div class="col-md-10">
-                <div class="panel panel-bordered panel-info">
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Form Details</h3>
-                        <div class="panel-actions">
-                            <a class="panel-collapse-icon voyager-angle-down" data-toggle="block-collapse"
-                               aria-hidden="true"></a>
-                        </div> <!-- /.panel-actions -->
-                    </div> <!-- /.panel-heading -->
-
-                    <div class="panel-body">
-                        <form
-                            role="form"
-                            action="@if (isset($form->id))
-                            {{ route('voyager.'.$dataType->slug.'.update', $form->id) }}
-                            @else
-                            {{ route('voyager.'.$dataType->slug.'.store') }}
-                            @endif"
-                            method="POST"
-                            enctype="multipart/form-data">
-
-                            {{ csrf_field() }}
-
-                            @if (isset($form->id))
-                                {{ method_field("PUT") }}
-                            @endif
-                            <div class="form-group">
-                                <label for="title">Title</label><br>
-                                <input name="title" class="form-control" type="text"
-                                       @if (isset($form->title)) value="{{ $form->title }}" @endif required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="mailto">Mail To (Separate multiple with ',')</label><br>
-                                <input name="mailto" class="form-control" type="text"
-                                       @if (isset($form->mailto)) value="{{ $form->mailto }}" @endif required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="hook">Event Hook (Fires after form is submitted)</label><br>
-                                <input name="hook" class="form-control" type="text"
-                                       @if (isset($form->hook)) value="{{ $form->hook }}" @endif>
-                            </div>
-
-                            <div class="panel-footer">
-                                <button type="submit"
-                                        class="btn btn-primary">{{ __('voyager.generic.submit') }}</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
+            <div class="col-md-8">
                 @if (isset($form))
                     @each('voyager-forms::inputs.edit-add', $form->inputs, 'input')
                 @endif
