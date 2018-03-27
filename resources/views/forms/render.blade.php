b2e0555e56d95e46474f892f62df7ec19cd14f84
Matt Mcnamee <matt@mcnamee.co>
2018-03-28 09:31:00 +1000

    Add default foundation styles to front-end form

--- resources/views/forms/render.blade.php
+++ resources/views/forms/render.blade.php
@@ -5,33 +5,45 @@
 
     @foreach ($form->inputs as $input)
         <div class="{{ $input->class }}">
-            <label for="{{ $input->label }}">{{ $input->label }}</label>
-
             @if (in_array($input->type, ['text', 'number', 'email']))
-                <input name="{{ $input->label }}" type="{{ $input->type }}" @if ($input->required) required @endif>
+                <label for="{{ $input->label }}">
+                    {{ $input->label }}
+                    <input name="{{ $input->label }}" type="{{ $input->type }}" @if ($input->required) required @endif>
+                </label>
             @endif
 
             @if ($input->type === 'text_area')
-                <textarea name="{{ $input->label }}" @if ($input->required) required @endif></textarea>
-            @endif
-
-            @if (in_array($input->type, ['radio', 'checkbox']))
-                @foreach (explode(', ', $input->options) as $option)
-                    <label for="{{ $option }}-{{ $input->type }}">{{ ucwords($option) }}
-                        <input id="{{ $option }}-{{ $input->type }}" name="{{ $input->label }}"
-                               type="{{ $input->type }}">
-                    </label>
-                @endforeach
+                <label for="{{ $input->label }}">
+                    {{ $input->label }}
+                    <textarea name="{{ $input->label }}" @if ($input->required) required @endif></textarea>
+                </label>
             @endif
 
             @if ($input->type === 'select')
-                <select name="{{ $input->label }}" @if ($input->required) required @endif>
-                    <option value="">-- Select --</option>
+                <label for="{{ $input->label }}">
+                    {{ $input->label }}
+                    <select name="{{ $input->label }}" @if ($input->required) required @endif>
+                        <option value="">-- Select --</option>
 
+                        @foreach (explode(', ', $input->options) as $option)
+                            <option value="{{ $option }}">{{ $option }}</option>
+                        @endforeach
+                    </select>
+                </label>
+            @endif
+
+            @if (in_array($input->type, ['radio', 'checkbox']))
+                <fieldset class="fieldset medium-12 cell">
+                    <legend>{{ $input->label }}</legend>
                     @foreach (explode(', ', $input->options) as $option)
-                        <option value="{{ $option }}">{{ $option }}</option>
+                        <input
+                            id="{{ $option }}-{{ $input->type }}"
+                            name="{{ $input->label }}"
+                            type="{{ $input->type }}"
+                        >
+                        <label for="{{ $option }}-{{ $input->type }}">{{ ucwords($option) }}</label>
                     @endforeach
-                </select>
+                </fieldset>
             @endif
         </div>
     @endforeach
